b626f0b8b4d75b64b32a0c4e51ad0dca87a75025
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-08-21 23:50:21 +0200

    TestToken constructor initialize DetailedERC20 base class

--- blackHole/contracts/TestToken.sol
+++ blackHole/contracts/TestToken.sol
@@ -3,8 +3,11 @@ pragma solidity ^0.4.22;
 import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 
-contract TestToken is StandardToken {
-    constructor() {
+contract TestToken is StandardToken, DetailedERC20 {
+    constructor(string _name, string _symbol, uint8 _decimals) 
+        DetailedERC20(_name, _symbol, _decimals) 
+        public 
+    {
 
     }
 }
