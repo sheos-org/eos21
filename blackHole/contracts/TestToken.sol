49e62a5181d1d30321f21b3b9a47b2cc7831399c
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-08-21 23:43:22 +0200

    TestToken contract created

---
+++ blackHole/contracts/TestToken.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.22;
+
+import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
+
+contract TestToken is StandardToken {
+    constructor() {
+
+    }
+}
