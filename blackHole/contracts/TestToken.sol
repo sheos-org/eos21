962ef4766584bf04bf0c1f461c7c625d0a286bc3
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-08-22 16:32:39 +0200

    TestToken is a MinteableToken

--- blackHole/contracts/TestToken.sol
+++ blackHole/contracts/TestToken.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.22;
 
-import "openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
 
-contract TestToken is StandardToken, DetailedERC20 {
+contract TestToken is MintableToken, DetailedERC20 {
     constructor(string _name, string _symbol, uint8 _decimals) 
         DetailedERC20(_name, _symbol, _decimals) 
         public 
