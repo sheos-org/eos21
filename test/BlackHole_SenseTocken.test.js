959ec255e450e44b657a20144ad824732b6ff70e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-08-24 15:08:44 +0200

    starting test with ERC20 contract

--- test/BlackHole_SenseTocken.test.js
+++ test/BlackHole_SenseTocken.test.js
@@ -17,8 +17,12 @@ contract('BlackHole_SenseTocken', accounts => {
     const nullAddress = 0x0;
     const note = 'no problems, just solutions';
 
+    let senseToken = null;
+    beforeEach(async () => {
+        senseToken = await SenseToken.new(name, symbol, tokens, decimals);
+    })
+
     it('create blackHole with ERC20 contract', async () => {
-        const senseToken = await SenseToken.new(name, symbol, tokens, decimals);
         const blackHole = await BlackHole.new(senseToken.address, genesisBlock);
     })
 });
