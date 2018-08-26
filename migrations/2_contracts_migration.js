304bcfa9e66972c6b068390044ae894f86f1ef9f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-08-26 12:50:07 +0200

    fix migration

--- migrations/2_contracts_migration.js
+++ migrations/2_contracts_migration.js
@@ -1,14 +1,15 @@
 var ElementToken = artifacts.require("./ElementToken.sol");
 var BlackHole = artifacts.require("./BlackHole.sol")
 
-module.exports = async function (deployer) {
+module.exports = function (deployer) {
   const name = 'Sense test';
   const symbol = 'SNS';
   const decimals = 8;
   const tokens = 100;
   const genesisBlock = 0;
 
-  await deployer.deploy(ElementToken, name, symbol, tokens, decimals);
-  await deployer.deploy(BlackHole, ElementToken.address, genesisBlock);
+  deployer.deploy(ElementToken, name, symbol, tokens, decimals).then(() => {
+    return deployer.deploy(BlackHole, ElementToken.address, genesisBlock);
+  })
 };
 
