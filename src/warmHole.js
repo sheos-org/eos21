c4595cddec3783392eda3cd4228faffbd6d485fe
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-08-26 14:25:22 +0200

    creating contract instance with BlackHole abi

--- src/warmHole.js
+++ src/warmHole.js
@@ -3,4 +3,7 @@ const Web3 = require('web3');
 
 const input = fs.readFileSync('./build/contracts/BlackHole.json');
 const abi = JSON.parse(input.toString()).abi;
-console.log(abi)
+
+let provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
+const web3 = new Web3(provider);
+let BlackHole = new web3.eth.Contract(abi);
