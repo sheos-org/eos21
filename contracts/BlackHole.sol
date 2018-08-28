b112483d450b528454aae4f54cec7cbb08d386ad
bensig <bensig@gmail.com>
2018-08-27 18:34:44 -0700

    Added ability to send to an existing EOS account + comments on code.

--- contracts/BlackHole.sol
+++ contracts/BlackHole.sol
@@ -2,21 +2,24 @@ pragma solidity ^0.4.22;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-contract BlackHole {
+// Blackhole contract - can set minimumAmount and critical block for ending ETH contract active period
+contract Blackhole {
     bool public closed = false;
     ERC20 public erc20Contract;
     uint public criticBlock;
     uint public minimumAmount;
 
+// Construction of the ETH Blackhole contract
     constructor(address _erc20Contract, uint _criticBlock, uint _minimumAmount) public {
         erc20Contract = ERC20(_erc20Contract);
         criticBlock = _criticBlock;
         minimumAmount = _minimumAmount;
     }
 
+// Check to make sure that the contract is still active if it has not reached the critical block expiration date
     function close() public {
-        require(!closed, "blackHole already closed");
-        require(block.number >= criticBlock, "blackHole hasn't reached the critical mass");
+        require(!closed, "This Blackhole contract's active period has expired.");
+        require(block.number >= criticBlock, "Blackhole hasn't reached the critical mass");
         closed = true;
     }
 
@@ -44,7 +47,8 @@ contract BlackHole {
         return true;
     }
 
-    function teleport(string eosPublicKey) public {
+// Use this function to move ERC20 tokens to a newly created EOS account associated with your public key
+    function teleportKey(string eosPublicKey) public {
         require(isValidKey(eosPublicKey), "not valid EOS public key");
         require(!closed, "blackHole closed");
         uint balance = erc20Contract.balanceOf(msg.sender);
@@ -52,10 +56,29 @@ contract BlackHole {
         require(allowed >= minimumAmount, "todo create message with minimumAmount");
         require(balance == allowed, "blackHole must attract all your tokens");
         require(erc20Contract.transferFrom(msg.sender, address(this), balance), "blackHole can't attract your tokens");
-        emit Teleport(balance, eosPublicKey);
+        emit TeleportKey(balance, eosPublicKey);
     }
 
-    event Teleport(
+// Use this function to move if a user has an existing EOS account, tokens can be moved via this method
+    function teleportAccount(string eosAccount) public {
+    // TODO add account eosAccount validation
+        require(!closed, "blackHole closed");
+        uint balance = erc20Contract.balanceOf(msg.sender);
+        uint allowed = erc20Contract.allowance(msg.sender, address(this));
+        require(allowed >= minimumAmount, "todo create message with minimumAmount");
+        require(balance == allowed, "blackHole must attract all your tokens");
+        require(erc20Contract.transferFrom(msg.sender, address(this), balance), "blackHole can't attract your tokens");
+        emit TeleportAccount(balance, eosAccount);
+    }
+
+// Activate teleportation of ERC20 Tokens to an existing EOS account via the src/wormhole.js
+    event TeleportAccount(
+        uint _tokens,
+        string _eosAccount
+    );
+
+// Activate teleportation of ERC20 Tokens to a new EOS account that will be created by the src/wormhole.js
+    event TeleportKey(
         uint _tokens,
         string _eosPublicKey
     );
