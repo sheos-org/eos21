fdca07100fd7d495d28665dc05ec64b220fbffc6
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-01 22:56:25 +0200

    eosValidator contract created

--- contracts/BlackHole.sol
+++ contracts/BlackHole.sol
@@ -2,27 +2,7 @@ pragma solidity ^0.4.22;
 
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 
-// BlackHole contract - can set minimumAmount and critical block for ending ETH contract active period
-contract BlackHole {
-    bool public closed = false;
-    ERC20 public erc20Contract;
-    uint public criticBlock;
-    uint public minimumAmount;
-
-// Construction of the ETH BlackHole contract
-    constructor(address _erc20Contract, uint _criticBlock, uint _minimumAmount) public {
-        erc20Contract = ERC20(_erc20Contract);
-        criticBlock = _criticBlock;
-        minimumAmount = _minimumAmount;
-    }
-
-// Check to make sure that the contract is still active if it has not reached the critical block expiration date
-    function close() public {
-        require(!closed, "This BlackHole contract's active period has expired.");
-        require(block.number >= criticBlock, "BlackHole hasn't reached the critical mass");
-        closed = true;
-    }
-
+contract EosValidator {
     function isValidKey(string str) public pure returns (bool){
         bytes memory b = bytes(str);
         if(b.length != 53) return false;
@@ -63,17 +43,40 @@ contract BlackHole {
         
         return true;
     }
+}
+
+// BlackHole contract - can set minimumAmount and critical block for ending ETH contract active period
+contract BlackHole {
+    EosValidator private eosValidator;
+    bool public closed = false;
+    ERC20 public erc20Contract;
+    uint public criticBlock;
+    uint public minimumAmount;
+
+// Construction of the ETH BlackHole contract
+    constructor(address _erc20Contract, uint _criticBlock, uint _minimumAmount) public {
+        erc20Contract = ERC20(_erc20Contract);
+        criticBlock = _criticBlock;
+        minimumAmount = _minimumAmount;
+    }
+
+// Check to make sure that the contract is still active if it has not reached the critical block expiration date
+    function close() public {
+        require(!closed, "This BlackHole contract's active period has expired.");
+        require(block.number >= criticBlock, "BlackHole hasn't reached the critical mass");
+        closed = true;
+    }
 
 // Use this function to move ERC20 tokens to a newly created EOS account associated with your public key
     function teleportKey(string eosPublicKey) public {
-        require(isValidKey(eosPublicKey), "not valid EOS public key");
+        require(eosValidator.isValidKey(eosPublicKey), "not valid EOS public key");
         uint amount = withdraw();
         emit TeleportKey(amount, eosPublicKey);
     }
 
 // Use this function to move if a user has an existing EOS account, tokens can be moved via this method
     function teleportAccount(string eosAccount) public {
-        require(isValidAccount(eosAccount), "not valid EOS account");
+        require(eosValidator.isValidAccount(eosAccount), "not valid EOS account");
         uint amount = withdraw();
         emit TeleportAccount(amount, eosAccount);
     }
@@ -90,7 +93,7 @@ contract BlackHole {
         string _eosPublicKey
     );
 
-    function withdraw() private returns (uint){
+    function withdraw() private returns (uint amount){
         require(!closed, "blackHole closed");
         uint balance = erc20Contract.balanceOf(msg.sender);
         uint allowed = erc20Contract.allowance(msg.sender, address(this));
