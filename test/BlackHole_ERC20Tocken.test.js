315dea1350ced375a8fba354b94c118ad731c4ac
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-01 22:31:35 +0200

    testing BlackHole::transferAccount

--- test/BlackHole_ERC20Tocken.test.js
+++ test/BlackHole_ERC20Tocken.test.js
@@ -16,15 +16,16 @@ contract('BlackHole_ERC20Tocken', accounts => {
 
     const criticBlock = 0;
     const eosPublicKey = 'EOS7M38bvCoL7N3mBDbQyqePcK128G2b3so7XBa9hJn9uuKDN7we8';
+    const eosAccount = "te.mgr5ymass";
 
-    it('teleportKey tokens', async () => {
+    it('teleport key', async () => {
         const erc20Token = await ERC20Token.new(name, symbol, tokens, decimals);
         const blackHole = await BlackHole.new(erc20Token.address, criticBlock, minimumAmount);
 
         let watcher = blackHole.TeleportKey();
 
         await erc20Token.approve(blackHole.address, 10000000000);
-        await blackHole.teleportKey(eosPublicKey)
+        await blackHole.teleportKey(eosPublicKey);
         const blackHoleBalance = await erc20Token.balanceOf(blackHole.address);
         blackHoleBalance.should.be.bignumber.equal(10000000000);
         const balance = await erc20Token.balanceOf(accounts[0]);
@@ -33,6 +34,25 @@ contract('BlackHole_ERC20Tocken', accounts => {
         const events = await watcher.get();
         events.length.should.be.equal(1);
         events[0].args._eosPublicKey.should.be.equal(eosPublicKey);
+        events[0].args._tokens.should.be.bignumber.equal(10000000000);
+    });
+
+    it('teleport account', async () => {
+        const erc20Token = await ERC20Token.new(name, symbol, tokens, decimals);
+        const blackHole = await BlackHole.new(erc20Token.address, criticBlock, minimumAmount);
+
+        let watcher = blackHole.TeleportAccount();
+
+        await erc20Token.approve(blackHole.address, 10000000000);
+        await blackHole.teleportAccount(eosAccount);
+        const blackHoleBalance = await erc20Token.balanceOf(blackHole.address);
+        blackHoleBalance.should.be.bignumber.equal(10000000000);
+        const balance = await erc20Token.balanceOf(accounts[0]);
+        balance.should.be.bignumber.equal(0);
+
+        const events = await watcher.get();
+        events.length.should.be.equal(1);
+        events[0].args._eosAccount.should.be.equal(eosAccount);
         events[0].args._tokens.should.be.bignumber.equal(10000000000);
     });
 
