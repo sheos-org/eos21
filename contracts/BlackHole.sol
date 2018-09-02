78ad4a54f50723e371e6e2ca26f9f2c0e641e944
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-02 09:11:30 +0200

    documentation

--- contracts/BlackHole.sol
+++ contracts/BlackHole.sol
@@ -5,7 +5,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
 /** @title BlackHole 
  * 
  * @dev Implementation of the BlackHole contract.
- * It deadlocks ERC20 tockens and emit events on burning.
+ * It deadlocks ERC20 tockens and emit events on success.
  */
 contract BlackHole {
     event Teleport(uint amount, string note);
@@ -15,20 +15,28 @@ contract BlackHole {
     uint public criticBlock;
     uint public minimumAmount;
 
-// Construction of the ETH BlackHole contract
+    /** @dev Construction of the ETH BlackHole contract.
+     * @param _erc20Contract The address of the ERC20 contract to attract tockens from.
+     * @param _criticBlock BlackHole can be closed after it.
+     * @param _minimumAmount the smallest amount BlackHole can attract.
+     */
     constructor(address _erc20Contract, uint _criticBlock, uint _minimumAmount) public {
         erc20Contract = ERC20(_erc20Contract);
         criticBlock = _criticBlock;
         minimumAmount = _minimumAmount;
     }
 
-// Check to make sure that the contract is still active if it has not reached the critical block expiration date
+    /** @dev Check to make sure that the contract is still active if it has not reached the critical block expiration date
+     */
     function close() public {
         require(!closed, "This BlackHole contract's active period has expired.");
         require(block.number >= criticBlock, "BlackHole hasn't reached the critical mass");
         closed = true;
     }
 
+    /** @dev teleport attracts tokens and emit Teleport event
+     * @param note Teleport event note.
+     */
     function teleport(string note) public {
         uint amount = attract();
         emit Teleport(amount, note);
--- contracts/EosBlackHole.sol
+++ contracts/EosBlackHole.sol
@@ -3,32 +3,38 @@ pragma solidity ^0.4.22;
 import "./EosValidator.sol";
 import "./BlackHole.sol";
 
-/** @title BlackHole 
+/** @title EosBlackHole 
  * 
- * @dev Implementation of the BlackHole contract.
- * It deadlocks ERC20 tockens and emit events on burning.
+ * @dev It can teleport tockens to EOS Public Key or EOS account.
  */
 contract EosBlackHole is EosValidator, BlackHole{
-// Activate teleportation of ERC20 Tokens to an existing EOS account via the src/wormhole.js
+    /** @dev Emitted when tokens are deadlocked.
+     *  @param tokens Amount.
+     *  @param eosAccount Associated EOS account.
+     */
     event TeleportAccount(uint tokens, string eosAccount);
 
-// Activate teleportation of ERC20 Tokens to a new EOS account that will be created by the src/wormhole.js
+    /** @dev Emitted when tokens are deadlocked.
+     *  @param tokens Amount.
+     *  @param eosPublicKey Associated EOS public key.
+     */
     event TeleportKey(uint tokens, string eosPublicKey);
 
-// Construction of the ETH BlackHole contract
     constructor(address _erc20Contract, uint _criticBlock, uint _minimumAmount) public 
     BlackHole(_erc20Contract, _criticBlock, _minimumAmount)
     {
     }
 
-// Use this function to move ERC20 tokens to a newly created EOS account associated with your public key
+    /** @dev It deadlocks your tokens and emit an event with amount and EOS public key.
+     */
     function teleportKey(string eosPublicKey) public {
         require(isValidKey(eosPublicKey), "not valid EOS public key");
         uint amount = attract();
         emit TeleportKey(amount, eosPublicKey);
     }
 
-// Use this function to move if a user has an existing EOS account, tokens can be moved via this method
+    /** @dev It deadlocks your tokens and emit an event with amount and EOS account.
+     */
     function teleportAccount(string eosAccount) public {
         require(isValidAccount(eosAccount), "not valid EOS account");
         uint amount = attract();
