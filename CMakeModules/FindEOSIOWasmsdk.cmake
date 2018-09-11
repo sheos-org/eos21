47e894f5e10be29e218fa9eca605cdf22cc1e7b8
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-11 14:46:52 +0200

    use the same basename for the abi file

--- CMakeModules/FindEOSIOWasmsdk.cmake
+++ CMakeModules/FindEOSIOWasmsdk.cmake
@@ -7,6 +7,8 @@ list(APPEND CMAKE_MODULE_PATH ${EOSIO_WASMSDK_ROOT}/lib/cmake)
 
 set(EOSIOWasmsdk_INCLUDE_DIRS ${EOSIO_WASMSDK_ROOT}/include)
 
+include_directories(${EOSIOWasmsdk_INCLUDE_DIRS})
+
 macro(target_abi TARGET OUTPUT)
         get_target_property(SOURCES ${TARGET} SOURCES)
         add_custom_command(TARGET ${TARGET} POST_BUILD
