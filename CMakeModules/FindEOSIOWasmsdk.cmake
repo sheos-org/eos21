ee16ea0bfbc3390bae264a44e4301016065c4baa
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-11 13:35:13 +0200

    target_abi created

--- CMakeModules/FindEOSIOWasmsdk.cmake
+++ CMakeModules/FindEOSIOWasmsdk.cmake
@@ -7,4 +7,14 @@ list(APPEND CMAKE_MODULE_PATH ${EOSIO_WASMSDK_ROOT}/lib/cmake)
 
 set(EOSIOWasmsdk_INCLUDE_DIRS ${EOSIO_WASMSDK_ROOT}/include)
 
+macro(target_abi TARGET OUTPUT)
+        get_target_property(SOURCES ${TARGET} SOURCES)
+        add_custom_command(TARGET ${TARGET} POST_BUILD
+                COMMAND ${EOSIOWasmsdk_ABIGEN} ${SOURCES} -output ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT}
+                COMMENT "Generating ABI ${OUTPUT}"
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                VERBATIM
+                )
+endmacro()
+
 
