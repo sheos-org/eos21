fdef77edfc094c112552159b2f1b6f7f3952e0e2
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-11 19:25:07 +0200

    added actions abi

--- whitehole/contracts/whitehole/whitehole.hpp
+++ whitehole/contracts/whitehole/whitehole.hpp
@@ -8,16 +8,20 @@ class whitehole {
 public:
     whitehole(account_name self);
 
+    [[eosio::action]]
     void create(account_name issuer, eosio::asset maximum_supply);
 
+    [[eosio::action]]
     void issue(account_name to, eosio::asset quantity, eosio::string memo);
 
     [[eosio::action]]
     bool iscreated() const;
 
+
 private:
     eosio::token mToken;
     bool mCreated = {false};
+    bool vbefasfawfawf;
 };
 
 EOSIO_ABI(whitehole, (issue))
