d1e9a45b4ba9dbf2d1203e915b2b1ac0ede5807e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-14 20:36:48 +0200

    [integration test] added helper function to deploy contracts

--- test/EosBlackHole_WarmHole.test.js
+++ test/EosBlackHole_WarmHole.test.js
@@ -16,22 +16,71 @@ const ganacheProvider = ganache.provider({
 });
 
 // set ganache to web3 as provider
-web3.setProvider(ganacheProvider); 
+web3.setProvider(ganacheProvider);
 
-const input = fs.readFileSync('./blackhole/build/contracts/EosBlackHole.json');
+// ERC20 Contract deploy
+const input = fs.readFileSync('./blackhole/build/contracts/ERC20Token.json');
 const contract = JSON.parse(input.toString());
 
-const blackHole = new web3.eth.Contract(contract.abi);
+const abi = contract.abi;
+const bytecode = contract.bytecode;
 
-const erc20ContractAddress = '0x000';
-const criticBlock = 0;
-const minimumAmount = 0;
-blackHole.deploy({data: contract.bytecode,
-    arguments: [erc20ContractAddress, criticBlock, minimumAmount]
-}).send({
-    from: identity.address,
-    gas: 1500000,
-    gasPrice: '30000000000000'
-}).then(instance => console.log(instance.options.address));
+const erc20Token = new web3.eth.Contract(abi);
 
-// const blackHole = BlackHole.new({from: identity.privateKey, data: contract.bytecode});
+function deployErc20Token() {
+    const name = 'ERC20 test';
+    const symbol = 'SNS';
+    const decimals = 8;
+    const tokens = 100;
+
+    try {
+        erc20Token.deploy({
+            data: bytecode,
+            arguments: [name, symbol, decimals, tokens]
+        })
+            .send({
+                from: identity.address,
+                gas: 1500,
+                gasPrice: '30'
+            })
+            .on('error', error => console.log)
+            .on('transactionHash', transactionHash => console.log)
+            .on('receipt', receipt => console.log(receipt.contractAddress))
+            .then(instance => console.log(instance.options.address));
+    } catch (e) {
+        console.log("Error in deplyment: " + e);
+    }
+};
+
+function deployBlackHole() {
+    // BlackHole deplay
+    const input = fs.readFileSync('./blackhole/build/contracts/EosBlackHole.json');
+    const contract = JSON.parse(input.toString());
+
+    const blackHole = new web3.eth.Contract(contract.abi);
+
+    const erc20ContractAddress = '0x000';
+    const criticBlock = 0;
+    const minimumAmount = 0;
+
+    try {
+        blackHole.deploy({
+            data: contract.bytecode,
+            arguments: [erc20ContractAddress, criticBlock, minimumAmount]
+        })
+            .send({
+                from: identity.address,
+                gas: 1500000,
+                gasPrice: '30000000000000'
+            })
+            .on('error', error => console.log)
+            .on('transactionHash', transactionHash => console.log)
+            .on('receipt', receipt => console.log(receipt.contractAddress))
+            .then(instance => console.log(instance.options.address));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+
+deployErc20Token();
