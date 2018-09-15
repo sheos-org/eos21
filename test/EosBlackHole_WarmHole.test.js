516e8d01a006886ff7978eabd8a479b06e8dd871
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-15 23:05:31 +0200

    [integration test] BlackHole deployed

--- test/EosBlackHole_WarmHole.test.js
+++ test/EosBlackHole_WarmHole.test.js
@@ -12,82 +12,70 @@ const ganacheProvider = ganache.provider({
     accounts: [{
         secretKey: identity.privateKey,
         balance: web3.utils.toWei('10', 'ether')
-    }]
+    }],
 });
 
 // set ganache to web3 as provider
 web3.setProvider(ganacheProvider);
 
-// ERC20 Contract deploy
-const input = fs.readFileSync('./blackhole/build/contracts/ERC20Token.json');
-const contract = JSON.parse(input.toString());
-
-const abi = contract.abi;
-const bytecode = contract.bytecode;
-
-const erc20Token = new web3.eth.Contract(abi);
-
-function deployErc20Token() {
+const deployErc20Token = () => {
+    const input = fs.readFileSync('./blackhole/build/contracts/ERC20Token.json');
+    const contract = JSON.parse(input.toString());
+    const abi = contract.abi;
+    const bytecode = contract.bytecode;
+    const erc20Token = new web3.eth.Contract(abi);
     const name = 'ERC20 test';
     const symbol = 'SNS';
     const decimals = 8;
     const tokens = 100;
 
-    try {
-        erc20Token.deploy({
-            data: bytecode,
-            arguments: [name, symbol, decimals, tokens]
+    return erc20Token.deploy({
+        data: bytecode,
+        arguments: [name, symbol, decimals, tokens]
+    })
+        .send({
+            from: identity.address,
+            gas: 3000000,
+            gasPrice: '20'
         })
-            .send({
-                from: identity.address,
-                gas: 1500,
-                gasPrice: '30'
-            })
-            .on('error', error => console.log)
-            .on('transactionHash', transactionHash => console.log)
-            .on('receipt', receipt => console.log(receipt.contractAddress))
-            .then(instance => console.log(instance.options.address));
-    } catch (e) {
-        console.log("Error in deplyment: " + e);
-    }
+        .on('error', error => console.log("Error: " + error))
+    //    .on('transactionHash', hash => console.log("Transaction: " + hash))
+    //    .on('receipt', receipt => console.log("Receipt :" + receipt))
+    //    .on('confirmation', (confirmationNumber, receipt) => console.log(confirmationNumber, receipt))
+
 };
 
-function deployBlackHole() {
-    // BlackHole deplay
+const deployBlackHole = (erc20ContractAddress) => {
     const input = fs.readFileSync('./blackhole/build/contracts/EosBlackHole.json');
     const contract = JSON.parse(input.toString());
 
     const blackHole = new web3.eth.Contract(contract.abi);
 
-    const erc20ContractAddress = '0x000';
     const criticBlock = 0;
     const minimumAmount = 0;
 
-    try {
-        blackHole.deploy({
-            data: contract.bytecode,
-            arguments: [erc20ContractAddress, criticBlock, minimumAmount]
+    return blackHole.deploy({
+        data: contract.bytecode,
+        arguments: [erc20ContractAddress, criticBlock, minimumAmount]
+    })
+        .send({
+            from: identity.address,
+            gas: 3000000,
+            gasPrice: '20'
         })
-            .send({
-                from: identity.address,
-                gas: 1500000,
-                gasPrice: '30000000000000'
-            })
-            .on('error', error => console.log)
-            .on('transactionHash', transactionHash => console.log)
-            .on('receipt', receipt => console.log(receipt.contractAddress))
-            .then(instance => console.log(instance.options.address));
-    } catch (e) {
-        console.log(e);
-    }
+        .on('error', console.log)
+//        .on('transactionHash', console.log)
+//        .on('receipt', receipt => console.log(receipt.contractAddress))
 };
 
-
-const erc20Token = deployErc20Token();
-const blackHole = deployBlackHole(erc20Token.address);
-const whiteHole = deployWhiteHole();
-const warmHole = new warmHole(blackHole.address, whiteHole.account);
-
+deployErc20Token().then(instance => {
+    const erc20TokenAddress = instance.options.address;
+    console.log("ERC20Contract address: " + erc20TokenAddress);
+    return deployBlackHole(erc20TokenAddress);
+}).then(instance => {
+    const blackHoleAddress = instance.options.address;
+    console.log("BlackHole address: " + blackHoleAddress);
+});
 
 
 
