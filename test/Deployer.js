5d96b10f70da3a090114c471f3e8856a3201846f
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-16 23:12:49 +0200

    refactoring : added Deployer

---
+++ test/Deployer.js
@@ -0,0 +1,52 @@
+const fs = require('fs');
+
+exports.deployErc20Token = (web3, identity) => {
+    const input = fs.readFileSync('./blackhole/build/contracts/ERC20Token.json');
+    const contract = JSON.parse(input.toString());
+    const abi = contract.abi;
+    const bytecode = contract.bytecode;
+    const erc20Token = new web3.eth.Contract(abi);
+    const name = 'ERC20 test';
+    const symbol = 'SNS';
+    const decimals = 8;
+    const tokens = 100;
+
+    return erc20Token.deploy({
+        data: bytecode,
+        arguments: [name, symbol, decimals, tokens]
+    })
+        .send({
+            from: identity.address,
+            gas: 3000000,
+            gasPrice: '20'
+        })
+        .on('error', error => console.log("Error: " + error))
+    //    .on('transactionHash', hash => console.log("Transaction: " + hash))
+    //    .on('receipt', receipt => console.log("Receipt :" + receipt))
+    //    .on('confirmation', (confirmationNumber, receipt) => console.log(confirmationNumber, receipt))
+
+};
+
+exports.deployBlackHole = (web3, identity, erc20ContractAddress) => {
+    const input = fs.readFileSync('./blackhole/build/contracts/EosBlackHole.json');
+    const contract = JSON.parse(input.toString());
+
+    const blackHole = new web3.eth.Contract(contract.abi);
+
+    const criticBlock = 0;
+    const minimumAmount = 0;
+
+    return blackHole.deploy({
+        data: contract.bytecode,
+        arguments: [erc20ContractAddress, criticBlock, minimumAmount]
+    })
+        .send({
+            from: identity.address,
+            gas: 3000000,
+            gasPrice: '20'
+        })
+        .on('error', console.log)
+    //        .on('transactionHash', console.log)
+    //        .on('receipt', receipt => console.log(receipt.contractAddress))
+};
+
--- test/EosBlackHole_WarmHole.test.js
+++ test/EosBlackHole_WarmHole.test.js
@@ -1,9 +1,9 @@
 const Web3 = require('web3');
 const ganache = require('ganache-cli');
 const EthCrypto = require('eth-crypto');
-const fs = require('fs');
 const WarmHole = require('../warmhole/WarmHole.js');
 require('chai').use(require('chai-as-promised')).should();
+const deployer = require('./Deployer.js');
 
 const web3 = new Web3();
 
@@ -13,7 +13,6 @@ for (let i = 0; i < identitiesCount; i++) {
     identities.push(EthCrypto.createIdentity());
 }
 
-console.log(identities.map(identity => ({secretKey: identity.privateKey, balance: web3.utils.toWei('10', 'ether') })))
 // create a ganache-provider
 const ganacheProvider = ganache.provider({
     // we preset the balance of our identities to 10 ether
@@ -23,73 +22,17 @@ const ganacheProvider = ganache.provider({
 // set ganache to web3 as provider
 web3.setProvider(ganacheProvider);
 
-const deployErc20Token = () => {
-    const input = fs.readFileSync('./blackhole/build/contracts/ERC20Token.json');
-    const contract = JSON.parse(input.toString());
-    const abi = contract.abi;
-    const bytecode = contract.bytecode;
-    const erc20Token = new web3.eth.Contract(abi);
-    const name = 'ERC20 test';
-    const symbol = 'SNS';
-    const decimals = 8;
-    const tokens = 100;
-
-    return erc20Token.deploy({
-        data: bytecode,
-        arguments: [name, symbol, decimals, tokens]
-    })
-        .send({
-            from: identities[0].address,
-            gas: 3000000,
-            gasPrice: '20'
-        })
-        .on('error', error => console.log("Error: " + error))
-    //    .on('transactionHash', hash => console.log("Transaction: " + hash))
-    //    .on('receipt', receipt => console.log("Receipt :" + receipt))
-    //    .on('confirmation', (confirmationNumber, receipt) => console.log(confirmationNumber, receipt))
-
-};
-
-const deployBlackHole = (erc20ContractAddress) => {
-    const input = fs.readFileSync('./blackhole/build/contracts/EosBlackHole.json');
-    const contract = JSON.parse(input.toString());
-
-    const blackHole = new web3.eth.Contract(contract.abi);
-
-    const criticBlock = 0;
-    const minimumAmount = 0;
-
-    return blackHole.deploy({
-        data: contract.bytecode,
-        arguments: [erc20ContractAddress, criticBlock, minimumAmount]
-    })
-        .send({
-            from: identities[0].address,
-            gas: 3000000,
-            gasPrice: '20'
-        })
-        .on('error', console.log)
-    //        .on('transactionHash', console.log)
-    //        .on('receipt', receipt => console.log(receipt.contractAddress))
-};
-
 describe('prova', async () => {
     // deploy the ERC20 contract
-    const erc20Contract = await deployErc20Token();
+    const erc20Contract = await deployer.deployErc20Token(web3, identities[0]);
     erc20Contract.should.not.equal(null);
-
-    // get the amount of erc20 tokens
-    let amount = await erc20Contract.methods.balanceOf(identities[0].address).call({from: identities[0].address});
-    //const partAmount = amount / identitiesCount;
 
     for (let i=0 ; i < identitiesCount ; i++){
         await erc20Contract.methods.transfer(identities[i].address, 10).send({from: identities[0].address});
     }
 
-
-
     // deploy BlackHole contract
-    const blackHoleContract = await deployBlackHole(erc20Contract.options.address);
+    const blackHoleContract = await deployer.deployBlackHole(web3, identities[0], erc20Contract.options.address);
     blackHoleContract.should.not.equal(null);
 
     // create WarmHole
@@ -107,6 +50,5 @@ describe('prova', async () => {
         result = await erc20Contract.methods.balanceOf(identities[i].address).call({ from: identities[i].address });
         result.should.be.equal('0');
     }
-    // console.log(result);
 });
 
