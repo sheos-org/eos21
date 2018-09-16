0136d3fdbd96e4895a6c412ff9fc5d2188d7aedc
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-16 22:58:12 +0200

    first part integration test works

--- test/EosBlackHole_WarmHole.test.js
+++ test/EosBlackHole_WarmHole.test.js
@@ -5,16 +5,19 @@ const fs = require('fs');
 const WarmHole = require('../warmhole/WarmHole.js');
 require('chai').use(require('chai-as-promised')).should();
 
-const identity = EthCrypto.createIdentity();
 const web3 = new Web3();
 
+var identities = []
+const identitiesCount = 10;
+for (let i = 0; i < identitiesCount; i++) {
+    identities.push(EthCrypto.createIdentity());
+}
+
+console.log(identities.map(identity => ({secretKey: identity.privateKey, balance: web3.utils.toWei('10', 'ether') })))
 // create a ganache-provider
 const ganacheProvider = ganache.provider({
-    // we preset the balance of our identity to 10 ether
-    accounts: [{
-        secretKey: identity.privateKey,
-        balance: web3.utils.toWei('10', 'ether')
-    }],
+    // we preset the balance of our identities to 10 ether
+    accounts: identities.map(identity => ({secretKey: identity.privateKey, balance: web3.utils.toWei('10', 'ether') })),
 });
 
 // set ganache to web3 as provider
@@ -36,7 +39,7 @@ const deployErc20Token = () => {
         arguments: [name, symbol, decimals, tokens]
     })
         .send({
-            from: identity.address,
+            from: identities[0].address,
             gas: 3000000,
             gasPrice: '20'
         })
@@ -61,7 +64,7 @@ const deployBlackHole = (erc20ContractAddress) => {
         arguments: [erc20ContractAddress, criticBlock, minimumAmount]
     })
         .send({
-            from: identity.address,
+            from: identities[0].address,
             gas: 3000000,
             gasPrice: '20'
         })
@@ -75,10 +78,15 @@ describe('prova', async () => {
     const erc20Contract = await deployErc20Token();
     erc20Contract.should.not.equal(null);
 
-    let amount = await erc20Contract.methods.balanceOf(identity.address).call({from: identity.address});
+    // get the amount of erc20 tokens
+    let amount = await erc20Contract.methods.balanceOf(identities[0].address).call({from: identities[0].address});
+    //const partAmount = amount / identitiesCount;
 
-    // TODO why following line doesn't work
-    //amount.should.be.equal(100000000);
+    for (let i=0 ; i < identitiesCount ; i++){
+        await erc20Contract.methods.transfer(identities[i].address, 10).send({from: identities[0].address});
+    }
+
+
 
     // deploy BlackHole contract
     const blackHoleContract = await deployBlackHole(erc20Contract.options.address);
@@ -89,12 +97,16 @@ describe('prova', async () => {
     warmHole.should.not.equal(null);
 
     // Check BlackHole is not closed
-    blackHoleContract.methods.closed().call({ from: identity.address }).should.eventually.be.false;
-   
-    result = await erc20Contract.methods.approve(blackHoleContract.options.address, amount).send({ from: identity.address });
-    result.status.should.be.true;
-    await blackHoleContract.methods.teleportToAccount("te.mgr5ymass").send({ from: identity.address });
-    result = await erc20Contract.methods.balanceOf(identity.address).call({ from: identity.address });
-   // console.log(result);
+    blackHoleContract.methods.closed().call({ from: identities[0].address }).should.eventually.be.false;
+
+    for (let i = 0; i < identitiesCount; i++) {
+        let amount = await erc20Contract.methods.balanceOf(identities[i].address).call({from: identities[i].address});
+        result = await erc20Contract.methods.approve(blackHoleContract.options.address, amount).send({ from: identities[i].address });
+        result.status.should.be.true;
+        await blackHoleContract.methods.teleportToAccount("te.mgr5ymass").send({ from: identities[i].address });
+        result = await erc20Contract.methods.balanceOf(identities[i].address).call({ from: identities[i].address });
+        result.should.be.equal('0');
+    }
+    // console.log(result);
 });
 
--- warmhole/WarmHole.js
+++ warmhole/WarmHole.js
@@ -1,17 +1,12 @@
 class WarmHole {
     constructor(blackHoleInstance) {
-        // console.log("Hello World!");
-
-
-        // console.log(blackHoleInstance.events);
-
-        // create the event
-
         blackHoleInstance.events.TeleportToAccount({
-            fromBlock: 0
+           // fromBlock: 0
         }, function (error, event) { console.log(event); })
             .on('data', function (event) {
-                console.log(event); // same results as the optional callback above
+                const { eosAccount, tokens } = event.returnValues;
+                console.log(eosAccount, tokens);
+                //console.log(event); // same results as the optional callback above
             })
             .on('changed', function (event) {
                 // remove event from local database
