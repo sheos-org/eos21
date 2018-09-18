6578ef11348b0107b65e924dae1efb80f4ab7be9
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-18 12:35:17 +0200

    using socken and listening for ctrl+c signal

--- eos21.js
+++ eos21.js
@@ -10,7 +10,22 @@ const whiteHoleAddress = argv.whitehole;
 console.log("(II) blackhole address is: " + blackHoleAddress);
 console.log("(II) whitehole address is: " + whiteHoleAddress);
 
+var zmq = require('zmq')
+    , socket = zmq.createSocket('rep');
 
-console.log("... ERC20 teleporting end.");
+socket.on('message', function (buf) {
+    // echo request back
+    socket.send(buf);
+});
+
+process.on('SIGINT', function () {
+    socket.close();
+    console.log("... ERC20 teleporting end.");
+    process.exit();
+});
+
+console.log("(II) press ctrl+c to exit");
+socket.bindSync('tcp://*:5555');
+
 
 
