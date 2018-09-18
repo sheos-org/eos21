4fc0c948d9757afa101feb26036bd08f4fa6c0dc
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-18 12:29:29 +0200

    more logs

--- eos21.js
+++ eos21.js
@@ -1,12 +1,16 @@
 const WormHole = require('./wormhole/WormHoleEosAccount.js');
 
+console.log("ERC20 teleporting start ...");
+
 const argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
 
 const blackHoleAddress = argv.blackhole;
 const whiteHoleAddress = argv.whitehole;
 
-console.log("blackhole address is: " + blackHoleAddress);
-console.log("whitehole address is: " + whiteHoleAddress);
+console.log("(II) blackhole address is: " + blackHoleAddress);
+console.log("(II) whitehole address is: " + whiteHoleAddress);
+
+
+console.log("... ERC20 teleporting end.");
 
 
