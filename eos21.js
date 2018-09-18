bbf8a52aade42c7f21065bb00ddb74201ef1e978
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-19 00:16:35 +0200

    better log

--- eos21.js
+++ eos21.js
@@ -1,4 +1,8 @@
 const socket = require('zmq').createSocket('rep');
+const Web3 = require('web3');
+const EosJs = require('eosjs');
+const fs = require('fs');
+
 const WormHole = require('./wormhole/WormHoleEosAccount.js');
 
 const wait = () => {
@@ -17,15 +21,35 @@ const wait = () => {
     socket.bindSync('tcp://*:5555');
 };
 
+const check = (condition, msg) => {
+    if (condition)
+        console.log("(II) " + msg);
+    else {
+        console.error("(EE) " + msg);
+        process.exit();
+    }
+}
+
 console.log("ERC20 teleporting starts ...");
 
 const argv = require('minimist')(process.argv.slice(2));
 
 const blackHoleAddress = argv.blackhole;
+check(blackHoleAddress, "blackhole address: " + blackHoleAddress);
 const whiteHoleAddress = argv.whitehole;
+check(whiteHoleAddress, "whitehole address: " + whiteHoleAddress);
+const web3Provider = argv.provider;
+check(web3Provider, "Ethereum provider: " + web3Provider);
 
-console.log("(II) blackhole address is: " + blackHoleAddress);
-console.log("(II) whitehole address is: " + whiteHoleAddress);
+const web3 = new Web3();
+const eosJs = new EosJs();
+
+web3.setProvider(new web3.providers.HttpProvider(web3Provider)); 
+const input = fs.readFileSync('./blackhole/build/contracts/ERC20Token.json');
+const contract = JSON.parse(input.toString());
+const abi = contract.abi;
+
+const blackHoleContract = web3.eth.Contract(abi, blackHoleAddress);
 
 wait();
 
