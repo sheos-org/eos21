d3416fbb753d6939477d168e103b5985b266936e
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-19 12:40:37 +0200

    using config file

--- eos21.js
+++ eos21.js
@@ -34,24 +34,29 @@ console.log("ERC20 teleporting starts ...");
 
 
 const argv = require('minimist')(process.argv.slice(2), {
-    string: ['blackhole', 'whitehole_key']
+    string: ['blackhole', 'whitehole_key'],
+    default: {
+        config: 'eos21.config'
+    }
 });
 
-const blackHoleAddress = Web3.utils.toChecksumAddress(argv.blackhole);
+const configFile = argv.config;
+
+check(fs.existsSync(configFile), "using config file " + configFile);
+const config = JSON.parse(fs.readFileSync(configFile));
+
+const { blackHoleAddress, whiteHoleAddress, ethereumProvider, whiteHoleKey, blackHoleFile } = config;
+
 check(blackHoleAddress, "blackhole address: " + blackHoleAddress);
-const whiteHoleAddress = argv.whitehole;
 check(whiteHoleAddress, "whitehole address: " + whiteHoleAddress);
-const web3Provider = argv.provider;
-check(web3Provider, "Ethereum provider: " + web3Provider);
-const whitehole_key = argv.whitehole_key; 
-check(whitehole_key, 'whitehole key: ' + whitehole_key);
-const blackHoleFile = './blackhole/build/contracts/BlackHoleEosAccount.json';
+check(ethereumProvider, "Ethereum provider: " + ethereumProvider);
+check(whiteHoleKey, 'whitehole key: ' + whiteHoleKey);
 check(fs.existsSync(blackHoleFile), "blackhole file: " + blackHoleFile);
 
 // Ethereum
 const web3 = new Web3();
 
-web3.setProvider(new web3.providers.HttpProvider(web3Provider)); 
+web3.setProvider(new web3.providers.HttpProvider(ethereumProvider)); 
 const input = fs.readFileSync(blackHoleFile);
 const contract = JSON.parse(input.toString());
 const abi = contract.abi;
@@ -59,14 +64,14 @@ const abi = contract.abi;
 check(web3.utils.isAddress(blackHoleAddress), "validating blackhole address");
 const blackHole = new web3.eth.Contract(abi, blackHoleAddress);
 check(blackHole, "create instance to blackhole contract");
-check(blackHole.options.address === blackHoleAddress, "instance has correct address");
+check(blackHole.options.address === web3.utils.toChecksumAddress(blackHoleAddress), "instance has correct address");
 
 // EOS 
 const eosJs = new EosJs();
 
-config = {
+eosConfig = {
     chainId: null, // 32 byte (64 char) hex string
-    keyProvider: [whitehole_key], // WIF string or array of keys..
+    keyProvider: [whiteHoleKey], // WIF string or array of keys..
     httpEndpoint: 'http://127.0.0.1:8888',
     expireInSeconds: 60,
     broadcast: true,
