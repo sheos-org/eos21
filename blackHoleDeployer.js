59de4b8b491c98bf17d855fe062cd0dc1368ef2c
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-28 10:29:31 +0200

    refactoring

--- blackHoleDeployer.js
+++ blackHoleDeployer.js
@@ -30,45 +30,20 @@ const BlackHole = new web3.eth.Contract(contract.abi);
 check(argv.erc20_address, "erc20_address: " + argv.erc20_address);
 check(argv.critic_block, "critic_block: " + argv.critic_block);
 check(argv.minimum_amount, "minimum_amount: " + argv.minimum_amount);
-const sender = '0x82cC3f53b9DD7Fc8F546DB9eBC497b8D69B1AebA';
+const sender = '0x259dFB6c0e57232184cAc7209Ba1032F755f925b';
 BlackHole.deploy({
     data: contract.bytecode,
     arguments: [argv.erc20_address, argv.critic_block, argv.minimum_amount]
 })
     .send({
         from: sender,
-        gas: 1500000,
-        gasPrice: '30000000000000'
-    }, console.log)
+        gas: 3000000,
+        gasPrice: '20'
+    })
     .on('error', error => console.log("(EE) " + error))
-    .on('transactionHash', console.log)
-    .on('receipt', receipt => console.log(receipt.contractAddress)) // contains the new contract address
-    .on('confirmation', (confirmationNumber, receipt) => console.log(confirmationNumber, receipt))
+    .on('transactionHash', transactionHash => console.log("(II) transactionHash: " + transactionHash))
+    .on('receipt', receipt => console.log("(II) receipt: \n" , receipt)) // contains the new contract address
+    .on('confirmation', (confirmationNumber, receipt) => console.log("(II) confirmation: " + confirmationNumber))
     .then(blackHole => {
-        console.log(blackHole.options.address) // instance with the new contract address
-
-        // Transaction has entered to geth memory pool
-        console.log("Your contract is being deployed in transaction at http://testnet.etherscan.io/tx/" + blackHole.transactionHash);
-
-        // http://stackoverflow.com/questions/951021/what-is-the-javascript-version-of-sleep
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
-
-        // We need to wait until any miner has included the transaction
-        // in a block to get the address of the contract
-        async function waitBlock() {
-            while (true) {
-                let receipt = web3.eth.getTransactionReceipt(blackHole.transactionHash);
-                if (receipt && receipt.contractAddress) {
-                    console.log("Your contract has been deployed at http://testnet.etherscan.io/address/" + receipt.contractAddress);
-                    console.log("Note that it might take 30 - 90 sceonds for the block to propagate befor it's visible in etherscan.io");
-                    break;
-                }
-                console.log("Waiting a mined block to include your contract... currently in block " + web3.eth.blockNumber);
-                await sleep(4000);
-            }
-        }
-
-        waitBlock();
+        console.log("(II) done");
     });
