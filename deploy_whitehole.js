0e5aac404b13197a6cb78cfaf23c9a648ccded96
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-30 21:55:00 +0200

    filling the help output

--- deploy_whitehole.js
+++ deploy_whitehole.js
@@ -6,19 +6,15 @@ const argv = require('minimist')(process.argv.slice(2), {
         wasm_file: './whitehole/build/contracts/whitehole.wasm',
         abi_file: './whitehole/build/contracts/whitehole.abi'
     },
-    boolean: ['help'],
-    string: ['erc20_address', 'sender']
+    boolean: ['help']
 });
 
 if (argv.help){
-    console.log("Help for BlackHole contract deployer:");
+    console.log("Help for WhiteHole contract deployer:");
     console.log("");
-    console.log("  --sender           address of account that is installing the contract");
-    console.log("  --erc20_address    address of erc20 contract blackhole will teleport from");
-    console.log("  --gas              amount of gas used in the transaction");
-    console.log("  --critic_block     after it anyone can close the blackhole");
-    console.log("  --minimum_amount   the minimum number of teportable tokens");
-    console.log("  --contract_file    compiled blackhole contract");
+    console.log("  --httpEndpoint     eos node endpoint");
+    console.log("  --wasm_file        whitehole wasm file");
+    console.log("  --abi_file         whitehole abi file");
     process.exit();
 }
 
