c851674bddb9aab3199b40fa0d3d8abf8d52715a
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-09-30 16:15:59 +0200

    EOSIOCDT -> EOSIO_CDT

--- whitehole/CMakeModules/FindEOSIOCDT.cmake
+++ whitehole/CMakeModules/FindEOSIOCDT.cmake
@@ -1,18 +1,18 @@
-find_program(EOSIOCDT_ABIGEN eosio-abigen PATHS ${EOSIOCDT_ROOT}/bin NO_DEFAULT_PATH)
+find_program(EOSIO_CDT_ABIGEN eosio-abigen PATHS ${EOSIO_CDT_ROOT}/bin NO_DEFAULT_PATH)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(EOSIOCDT REQUIRED_VARS EOSIOCDT_ABIGEN)
+find_package_handle_standard_args(EOSIO_CDT REQUIRED_VARS EOSIO_CDT_ABIGEN)
 
-list(APPEND CMAKE_MODULE_PATH ${EOSIOCDT_ROOT}/lib/cmake)
+list(APPEND CMAKE_MODULE_PATH ${EOSIO_CDT_ROOT}/lib/cmake)
 
-set(EOSIO.cdt_INCLUDE_DIRS ${EOSIOCDT_ROOT}/include)
+set(EOSIO.cdt_INCLUDE_DIRS ${EOSIO_CDT_ROOT}/include)
 
-include_directories(${EOSIOCDT_INCLUDE_DIRS})
+include_directories(${EOSIO_CDT_INCLUDE_DIRS})
 
 macro(target_generate_abi TARGET OUTPUT)
     get_target_property(SOURCES ${TARGET} SOURCES)
     add_custom_command(TARGET ${TARGET} POST_BUILD
-	    COMMAND ${EOSIOCDT_ABIGEN} ${SOURCES} -output ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT}
+            COMMAND ${EOSIO_CDT_ABIGEN} ${SOURCES} -output ${CMAKE_CURRENT_BINARY_DIR}/${OUTPUT}
         COMMENT "Generating ABI ${OUTPUT}"
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         VERBATIM
