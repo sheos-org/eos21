ca8d8b25ca4cde5e35e6821e8f8c289bd50dc271
Alessandro Siniscalchi <asiniscalchi@gmail.com>
2018-10-04 16:14:53 +0200

    deploy_blackhole using the chainid and the privatekey

--- utils/WhiteHoleDeployer.js
+++ utils/WhiteHoleDeployer.js
@@ -4,7 +4,16 @@ const check = require('./Check');
 
 module.exports = (argv) => {
     check(argv.http_endpoint, "http_endpoint: " + argv.http_endpoint);
-    let eos = EosJs(argv);
+    check(argv.private_key, "private_key: " + argv.private_key);
+    const eos = EosJs({
+        chainId: argv.chain_id, // 32 byte (64 char) hex string
+        keyProvider: [argv.private_key],
+        httpEndpoint: argv.http_endpoint,
+        expireInSeconds: 60,
+        broadcast: true,
+        verbose: false, // API activity
+        sign: true
+    });
 
     const { wasm_file, abi_file, account } = argv;
     check(fs.existsSync(wasm_file), "wasm file: " + wasm_file);
@@ -18,5 +27,7 @@ module.exports = (argv) => {
             console.log(result);
             return eos.setabi(account, JSON.parse(abi));
         })
-        .then(console.log);
+        .catch(console.log)
+        .then(console.log)
+        .catch(console.log);
 }
