cae15f7b851761ccd200c6499588a0e485bfc7b5
Angel Jose <ajose01@gmail.com>
2019-01-10 17:24:03 -0800

    Alternative using timestamps to approximate teleport duration

---
+++ blackhole/migrations/2_contract_ts_migration.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const check = require('../../utils/Check');
+
+var ERC20Token = artifacts.require("./ERC20Token.sol");
+var BlackHole = artifacts.require("./BlackHoleEosAccount.sol")
+
+module.exports = function (deployer) {
+  const configFile = "../config.json";
+  //check(fs.existsSync(configFile), "configuration file: " + configFile);
+  const config = JSON.parse(fs.readFileSync(configFile));
+  //console.log(config)
+  const name = "ERC20 Test";
+  const symbol = config.blackhole.symbol;
+  const decimals = config.blackhole.decimals;
+  const tokens = config.blackhole.tokens;
+  //const genesisBlock = config.blackhole.critic_block;
+  const duration = config.blackhole.minDuration;
+  const minimumAmount = config.blackhole.minimum_amount;
+
+  check(name, "ERC20 name: " + name);
+  check(symbol, "ERC20 symbol: " + symbol);
+  check(tokens, "ERC20 tokens: " + tokens);
+  check(decimals, "ERC20 decimals: " + decimals);
+  //check(genesisBlock, "BlackHole critical block: " + genesisBlock);
+  check(duration, "BlackHole duration: " + duration);
+  check(minimumAmount, "BlackHole minimum amount: " + minimumAmount);
+
+  deployer.deploy(ERC20Token, name, symbol, tokens, decimals).then(() => {
+    return deployer.deploy(BlackHole, ERC20Token.address, duration, minimumAmount);
+  })
+    .then(() => {
+      fs.writeFileSync('../erc20_address', ERC20Token.address);
+      fs.writeFileSync('../blackhole_address', BlackHole.address);
+    })
+};
+
